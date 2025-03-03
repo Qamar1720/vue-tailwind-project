51e4ba3d1d805b400fbffc7bb2b1473086cfd19c
Qamar1720 <iqamar@onyxtec.co>
2025-03-03 13:20:33 +0500

    adding finalize card responsiveness and pagination

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,79 +1,96 @@
 <template>
     <div class="bg-[#1A0B2E] text-white px-8 py-12 font-oswald w-full">
-        <div class="flex flex-col lg:flex-row lg:justify-between max-w-[1440px] w-full mx-auto">
+        <div class="lg:flex lg:gap-20 max-w-[1440px] w-full mx-auto">
             <!-- Left Section -->
-            <div class="flex flex-col gap-6 max-w-sm">
+            <div class="flex flex-col gap-6 w-full lg:w-[35%]">
                 <img src="../assets/pics/logo.png" alt="Logo" class="w-54">
-                <p class="text-gray-300 text-lg font-oswald overflow-hidden">
+                <p class="text-gray-300 text-lg font-oswald">
                     Discover and explore an immersive world of new maps, talented creators, and exhilarating experiences tailored for Fortnite players on this innovative platform.
                 </p>
                 <div class="relative w-full">
                     <input type="email" placeholder="Email address"
-                        class="w-full p-3 bg-transparent border border-gray-500 rounded-md placeholder-gray-400">
-                    <span class="absolute right-4 top-1/2 transform -translate-y-1/2 text-red-500 text-4xl font-bold">→</span>
+                        class="w-full p-6 font-bold text-gray-400 bg-transparent border border-gray-500 rounded-md placeholder-gray-400">
+                    <span class="absolute right-4 top-10 transform -translate-y-3/4 text-red-500 text-4xl font-bold">→</span>
                 </div>
             </div>
 
             <!-- Center Sections -->
-            <div class="flex md:flex-row flex-col gap-18 mt-6 lg:mt-0 max-w-[1440px] w-full">
+            <div class="flex sm:flex-row sm:justify-around flex-col pl-0 lg:gap-8 gap-8 mt-10 lg:mt-0 w-full lg:w-[40%]">
                 <div class="flex flex-col gap-4">
                     <h2 class="text-lg font-semibold mb-4">EXPLORE</h2>
-                    <ul class="space-y-2 list-disc list-inside">
-                        <li class="text-gray-400 hover:text-red-500">About</li>
-                        <li class="text-gray-400 hover:text-red-500">Discover Live</li>
-                        <li class="text-gray-400 hover:text-red-500">Categories</li>
-                        <li class="text-gray-400 hover:text-red-500">Consulting</li>
-                        <li class="text-gray-400 hover:text-red-500">Contact</li>
+                    <ul class="space-y-2 list-disc list-inside  cursor-pointer">
+                        <li class="text-gray-400 marker:text-[#D93E31] hover:text-[#D93E31]">About</li>
+                        <li class="text-gray-400 marker:text-[#D93E31] hover:text-[#D93E31]">Discover Live</li>
+                        <li class="text-gray-400 marker:text-[#D93E31] hover:text-[#D93E31]">Categories</li>
+                        <li class="text-gray-400 marker:text-[#D93E31] hover:text-[#D93E31]">Consulting</li>
+                        <li class="text-gray-400 marker:text-[#D93E31] hover:text-[#D93E31]">Contact</li>
                     </ul>
                 </div>
                 <div class="flex flex-col gap-4">
                     <h2 class="text-lg font-semibold mb-4">RESOURCES</h2>
-                    <ul class="space-y-2 list-disc list-inside">
-                        <li class="text-gray-400 hover:text-red-500">News & Updates</li>
-                        <li class="text-gray-400 hover:text-red-500">Top Studios</li>
-                        <li class="text-gray-400 hover:text-red-500">Top Developers</li>
-                        <li class="text-gray-400 hover:text-red-500">Event Calendars</li>
-                        <li class="text-gray-400 hover:text-red-500">Skin Databases</li>
+                    <ul class="space-y-2 list-disc list-inside cursor-pointer">
+                        <li class="text-gray-400 marker:text-[#D93E31] hover:text-[#D93E31]">News & Updates</li>
+                        <li class="text-gray-400 marker:text-[#D93E31] hover:text-[#D93E31]">Top Studios</li>
+                        <li class="text-gray-400 marker:text-[#D93E31] hover:text-[#D93E31]">Top Developers</li>
+                        <li class="text-gray-400 marker:text-[#D93E31] hover:text-[#D93E31]">Event Calendars</li>
+                        <li class="text-gray-400 marker:text-[#D93E31] hover:text-[#D93E31]">Skin Databases</li>
                     </ul>
                 </div>
             </div>
-
-            <!-- Right Section -->
-            <div class="mt-8 lg:mt-0 flex flex-col gap-7 max-w-[1440px] w-full">
-                <div>
-                    <p class="text-gray-400 mb-2">Phone </p>
-                    <p class="font-bold">+1 234 5678 910</p>
-                </div>
-                <div>
-                    <p class="text-gray-400 mb-2">Address </p>
-                    <p class="font-bold">USA</p>
-                </div>
-                <div>
-                    <p class="text-gray-400">Email Address</p>
-                    <p class="">NEEDHELP@FORTNITEDATA.COM</p>
-                </div>
+                
+                <!-- Right Section -->
+                <div class="mt-8 lg:mt-0 flex flex-col md:justify-start gap-7 w-full lg:w-[25%]">
+                    <div>
+                        <p class="text-gray-400 mb-2">Phone</p>
+                        <p class="font-bold">+1 234 5678 910</p>
+                    </div>
+                    <div>
+                        <p class="text-gray-400 mb-2">Address</p>
+                        <p class="font-bold">USA</p>
+                    </div>
+                    <div>
+                        <p class="text-gray-400 mb-2">Email Address</p>
+                        <p class="font-bold break-words lg:max-w-[85%]">NEEDHELP@FORTNITEDATA.COM</p>
+                    </div>
             </div>
         </div>
 
         <!-- Copyright -->
-        <div class="relative mt-12 pt-6 text-center text-gray-400 flex md:flex-row flex-col gap-4 md:gap-0 justify-around bg-[#1A0B2E]">
-            <div class="flex gap-6 justify-center items-center text-2xl text-white cursor-pointer">
-                <div class="border-2 rounded-full p-2 border-[#3E344B] hover:text-[#DF3F2A] hover:shadow-[#DF3F2A] hover:shadow-[0_0_22px_0px] px-4">
-                    <i class="fa fa-twitter"></i>
-                </div>
-                <div class="border-2 rounded-full p-2 border-[#3E344B] hover:text-[#DF3F2A] shadow-[#DF3F2A] hover:shadow-[0_0_22px_2px] px-4">
-                    <i class="fa fa-facebook"></i>
-                </div>
-                <div class="border-2 rounded-full p-2 border-[#3E344B] hover:text-[#DF3F2A] shadow-[#DF3F2A] hover:shadow-[0_0_22px_2px] px-4">
-                    <i class="fa fa-pinterest"></i>
-                </div>
-                <div class="border-2 rounded-full p-2 border-[#3E344B] hover:text-[#DF3F2A] shadow-[#DF3F2A] hover:shadow-[0_0_22px_2px] px-4">
-                    <i class="fa fa-instagram"></i>
-                </div>
-            </div>
-            <div>
+        <div class="relative mt-12 pt-6 text-center text-gray-400 flex md:flex-row flex-col gap-4 md:gap-0 justify-between bg-[#1A0B2E] border-2 border-[#3E344B] max-w-[1440px] mx-auto rounded-lg p-2 border-b-transparent">
+            <div class="flex items-center justify-center">
                 &copy; Copyrights, 2023 Fortnitedata.com
+            </div>
+            <div class="flex sm:justify-center  flex-wrap gap-4 md:gap-5 lg:gap-6 text-xl md:text-2xl text-white cursor-pointer px-2">
+            <div class="border-2 lg:px-3 rounded-full px-2 md:px-3 md:py-2 lg:p-2 border-[#3E344B] hover:text-[#DF3F2A] hover:shadow-[#DF3F2A] hover:shadow-[0_0_22px_0px]">
+                <i class="fa fa-twitter"></i>
+            </div>
+            <div class="border-2 lg:px-4 rounded-full px-2 md:px-4 md:py-2 lg:p-2 border-[#3E344B] hover:text-[#DF3F2A] hover:shadow-[#DF3F2A] hover:shadow-[0_0_22px_2px]">
+                <i class="fa fa-facebook"></i>
+            </div>
+            <div class="border-2 lg:px-3 rounded-full px-2 md:px-3 md:py-2 lg:p-2 border-[#3E344B] hover:text-[#DF3F2A] hover:shadow-[#DF3F2A] hover:shadow-[0_0_22px_2px]">
+                <i class="fa fa-pinterest"></i>
+            </div>
+            <div class="border-2 lg:px-3 rounded-full px-2 md:px-3 md:py-2 lg:p-2 border-[#3E344B] hover:text-[#DF3F2A] hover:shadow-[#DF3F2A] hover:shadow-[0_0_22px_2px]">
+                <i class="fa fa-instagram"></i>
+            </div>
             </div>
         </div>
     </div>
 </template>
+<style scoped>
+  /* .trapezoid {
+  position: relative;
+  width: full; 
+  height: 70px; 
+  clip-path: polygon(10% 10%, 100% 0%, 85% 100%, 15% 100%);
+  display: flex;
+  border-color: #3E344B;
+  align-items: center;
+  justify-content: center;
+} */
+ @media screen and (min-width: 1024px) and (max-width: 1100px) {
+    .email{
+     width: 80%;
+    }
+ }
+</style>
--- src/components/MainNavbar.vue
+++ src/components/MainNavbar.vue
@@ -1,10 +1,13 @@
 <template>
+  <div>
+    <hr class="text-[#3E344B]">
+  </div>
 <div class=" w-full flex min-w-full bg-[#1A0B2E] min-h-screen relative pt-8">
   <!-- Background Image -->
   <div class="w-full flex items-center justify-center relative">
     <img src="/home/test/Documents/Demo project/vue-tailwind-project/src/assets/pics/header_image.jpg" alt="" class="w-full">
    </div>
-   <div class="max-w-[1440px] w-full mx-auto flex justify-between items-center absolute top-4 left-1/2 -translate-x-1/2 px-6 z-10">
+   <div class="max-w-[1440px] w-full mx-auto flex md:flex-col md:gap-5 xl:gap-0 xl:flex-row justify-between items-center absolute top-4 left-1/2 -translate-x-1/2 px-6 z-10 listitems">
     <!-- Logo -->
     <div class="cursor-pointer w-auto">
       <img src="../assets/pics/logo.png" alt="Logo" class="h-12 w-auto" />
@@ -13,18 +16,28 @@
     
     <!-- Navigation Links -->
     <div>
-      <ul class="hidden md:flex gap-13 mt-2 text-white  font-bold">
-        <li class="transition duration-300 cursor-pointer hover:text-[#DF3F2A]  hover:shadow-[0_0_25px_2px] hover:rounded-full hover:shadow-[#33132A]">HOME</li>
-        <li class="transition duration-300 cursor-pointer hover:text-[#DF3F2A] hover:shadow-[0_0_25px_2px] hover:shadow-[#33132A] hover:rounded-full ">DISCOVER LIVE</li>
-        <li class="transition duration-300 cursor-pointer hover:text-[#DF3F2A] hover:shadow-[0_0_25px_2px] hover:rounded-full hover:shadow-[#33132A]">CATEGORIES</li>
-        <li class="transition duration-300 cursor-pointer hover:text-[#DF3F2A] hover:shadow-[0_0_25px_2px] hover:rounded-full hover:shadow-[#33132A]">CONSULTING</li>
-      <li class="transition duration-300 cursor-pointer hover:text-[#DF3F2A] 
-       hover:shadow-[0_0_25px_2px] hover:rounded-full hover:shadow-[#33132A]">NEWS & UPDATES</li>
-    </ul>
-  </div>
+      <ul class="hidden md:flex gap-13 mt-2 text-white font-bold">
+        <li class="relative transition duration-300 cursor-pointer hover:text-[#DF3F2A] hover:shadow-[0_0_25px_2px] hover:rounded-full hover:shadow-[#33132A] before:absolute before:top-[-34px] before:left-0 before:w-full before:h-[8px] before:bg-[#DF3F2A] before:transition-all before:duration-300 before:ease-in-out before:scale-0 hover:before:scale-100">
+          HOME
+        </li>
+        <li class="relative transition duration-300 cursor-pointer hover:text-[#DF3F2A] hover:shadow-[0_0_25px_2px] hover:rounded-full hover:shadow-[#33132A] before:absolute before:top-[-34px] before:left-0 before:w-full before:h-[8px] before:bg-[#DF3F2A] before:transition-all before:duration-300 before:ease-in-out before:scale-0 hover:before:scale-100">
+          DISCOVER LIVE
+        </li>
+        <li class="relative transition duration-300 cursor-pointer hover:text-[#DF3F2A] hover:shadow-[0_0_25px_2px] hover:rounded-full hover:shadow-[#33132A] before:absolute before:top-[-34px] before:left-0 before:w-full before:h-[8px] before:bg-[#DF3F2A] before:transition-all before:duration-300 before:ease-in-out before:scale-0 hover:before:scale-100">
+          CATEGORIES
+        </li>
+        <li class="relative transition duration-300 cursor-pointer hover:text-[#DF3F2A] hover:shadow-[0_0_25px_2px] hover:rounded-full hover:shadow-[#33132A] before:absolute before:top-[-34px] before:left-0 before:w-full before:h-[8px] before:bg-[#DF3F2A] before:transition-all before:duration-300 before:ease-in-out before:scale-0 hover:before:scale-100">
+          CONSULTING
+        </li>
+        <li class="relative transition duration-300 cursor-pointer hover:text-[#DF3F2A] hover:shadow-[0_0_25px_2px] hover:rounded-full hover:shadow-[#33132A] before:absolute before:top-[-34px] before:h-[8px] before:w-full before:bg-[#DF3F2A] before:scale-0 before:transition-all before:duration-300 before:ease-in-out hover:before:scale-100">
+          NEWS & UPDATES
+        </li>
+      </ul>
+    </div>
+
   
   <!-- Hamburger Menu and Close Icon -->
-  <div class="md:hidden flex gap-4 text-white text-3xl ">
+  <div class="md:hidden flex gap-4 text-[#3E344B] text-3xl ">
     <i :class="isMenuOpen ? 'fa fa-times' : 'fa fa-bars'" 
     class="cursor-pointer" 
     @click="toggleMenu">
@@ -41,17 +54,17 @@
     <li class="hover:text-[#DF3F2A] hover:rounded-full cursor-pointer">NEWS & UPDATES</li>
   </ul>
 </div>
-<div class="hidden lg:flex flex-wrap gap-4 md:gap-5 lg:gap-6 text-xl md:text-2xl text-white cursor-pointer">
-  <div class="border-2 rounded-full p-2 md:p-3 lg:p-2 border-[#3E344B] hover:text-[#DF3F2A] hover:shadow-[#DF3F2A] hover:shadow-[0_0_22px_0px]">
+<div class="hidden md:flex flex-wrap gap-4 md:gap-5 lg:gap-6 text-xl md:text-2xl text-white cursor-pointer px-2">
+  <div class="border-2 lg:px-3 rounded-full px-2 md:px-3 md:py-2 lg:p-2 border-[#3E344B] hover:text-[#DF3F2A] hover:shadow-[#DF3F2A] hover:shadow-[0_0_22px_0px]">
     <i class="fa fa-twitter"></i>
   </div>
-  <div class="border-2 rounded-full p-2 md:p-3 lg:p-2 border-[#3E344B] hover:text-[#DF3F2A] hover:shadow-[#DF3F2A] hover:shadow-[0_0_22px_2px]">
+  <div class="border-2 lg:px-4 rounded-full px-2 md:px-4 md:py-2 lg:p-2 border-[#3E344B] hover:text-[#DF3F2A] hover:shadow-[#DF3F2A] hover:shadow-[0_0_22px_2px]">
     <i class="fa fa-facebook"></i>
   </div>
-  <div class="border-2 rounded-full p-2 md:p-3 lg:p-2 border-[#3E344B] hover:text-[#DF3F2A] hover:shadow-[#DF3F2A] hover:shadow-[0_0_22px_2px]">
+  <div class="border-2 lg:px-3 rounded-full px-2 md:px-3 md:py-2 lg:p-2 border-[#3E344B] hover:text-[#DF3F2A] hover:shadow-[#DF3F2A] hover:shadow-[0_0_22px_2px]">
     <i class="fa fa-pinterest"></i>
   </div>
-  <div class="border-2 rounded-full p-2 md:p-3 lg:p-2 border-[#3E344B] hover:text-[#DF3F2A] hover:shadow-[#DF3F2A] hover:shadow-[0_0_22px_2px]">
+  <div class="border-2 lg:px-3 rounded-full px-2 md:px-3 md:py-2 lg:p-2 border-[#3E344B] hover:text-[#DF3F2A] hover:shadow-[#DF3F2A] hover:shadow-[0_0_22px_2px]">
     <i class="fa fa-instagram"></i>
   </div>
 </div>
@@ -74,3 +87,12 @@ export default {
 }
 };
 </script>
+<style scoped>
+  @media screen and (min-width:1279px) and (max-width:1290px){
+  .listitems{
+      display: flex;
+      flex-direction: column;
+      gap: 14;
+    }
+  }
+</style>
